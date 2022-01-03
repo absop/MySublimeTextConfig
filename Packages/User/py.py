ad38495adf78e6dd8bdce25385501f482fbe8ed0
叶芝秋 <zlang0@163.com>
2022-01-04 02:03:13 +0800

    Delete py.py

--- Packages/User/py.py
+++
@@ -1,7 +0,0 @@
-import os
-
-dir = 'D:/SublimeText3/Data/Packages/User'
-for file in os.listdir():
-    path = os.path.join(dir, file)
-    if path.endswith('(Flake8Lint).tmTheme'):
-        os.remove(path)
